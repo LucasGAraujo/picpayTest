99c5c5834b9e3ec56e5fda0e5d4ba64a348c6e88
LucasGaraujo <lucasdag445@gmail.com>
2024-01-05 17:32:32 -0300

    feat: initial

---
+++ src/test/java/com/picpaytest/picpaytest/PicpaytestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpaytest.picpaytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpaytestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
